249a1b5a08d7c1cbb477e35e44cbb6769e0f1ad3
Sojib Ali <sojib.ali@hotmail.com>
2024-01-02 23:38:23 +0600

    Section-2: Start a New React Project

--- react_courses/src/App.js
+++ react_courses/src/App.js
@@ -1,10 +1,30 @@
+import Navbar from './components/Navbar';
+import Card from "./components/Card";
 import './App.css';
+
+const photos=[
+  'https://picsum.photos/id/1001/200/200',
+  'https://picsum.photos/id/1002/200/200',
+  'https://picsum.photos/id/1003/200/200',
+  'https://picsum.photos/id/1004/200/200',
+  'https://picsum.photos/id/1005/200/200',
+  'https://picsum.photos/id/1006/200/200',
+  'https://picsum.photos/id/1009/200/200',
+  'https://picsum.photos/id/1008/200/200',
+]
 
 function App() {
   return (
-    <div className="App">
-      <h1>Happy New Year!2024!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div class="container text-center mt-5">
+        <h1>Gallery</h1>
+        <div className='row'>
+          {/* {Array.apply(null,{length: 9}).map(()=> <Card />)} */}
+          {photos.map((photo)=> <Card src={photo}/>)}
+        </div>
+      </div>
+    </>
   );
 }
 export default App;
