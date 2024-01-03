9c5840b2d7dc294a96ebbe05d334202e4ea9eab9
Sojib Ali <sojib.ali@hotmail.com>
2024-01-03 23:54:38 +0600

    till work with forms

--- react_courses/src/App.js
+++ react_courses/src/App.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Card from "./components/Card";
+import UploadForm from "./components/UploadForm";
 import './App.css';
 
 const photos=[
@@ -9,19 +11,36 @@ const photos=[
   'https://picsum.photos/id/1004/200/200',
   'https://picsum.photos/id/1005/200/200',
   'https://picsum.photos/id/1006/200/200',
-  'https://picsum.photos/id/1009/200/200',
-  'https://picsum.photos/id/1008/200/200',
 ]
 
 function App() {
+  const [input, setInput]=useState();
+  const [items, setItems]=useState(photos);
+  const [isCollapsed, collapse]= useState(false);
+
+  const toggle=()=> collapse(!isCollapsed);
+  const handleOnChange=(e)=>setInput(e.target.value);
+  const handleOnSubmit=(e)=>{
+    e.preventDefault();
+    setItems([input, ...items]);
+  }
+
   return (
     <>
       <Navbar />
       <div class="container text-center mt-5">
-        <h1>Gallery</h1>
+      {/* <button className="btn btn-warning" onClick={() => setItems(['https://picsum.photos/id/1008/200/200', ...items])}> +Add</button><br /> */}
+        <button className='btn btn-success float-end' onClick={toggle} >{isCollapsed ? 'Close':'Add'}</button>
+        <div className='clearfix mb-4'></div>
+        <UploadForm 
+        isVisible={isCollapsed}
+        onChange={handleOnChange}
+        onSubmit={handleOnSubmit}
+        />
+        <h1 className='clearfix mb-4'>Gallery</h1>
         <div className='row'>
           {/* {Array.apply(null,{length: 9}).map(()=> <Card />)} */}
-          {photos.map((photo)=> <Card src={photo}/>)}
+          {items.map((photo)=> <Card src={photo}/>)}
         </div>
       </div>
     </>
