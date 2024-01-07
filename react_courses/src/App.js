7ba2e3c259394d218a7152842bd1b2694df51de8
Sojib Ali <sojib.ali@hotmail.com>
2024-01-07 23:57:39 +0600

    manage the state of your app with the Context API

--- react_courses/src/App.js
+++ react_courses/src/App.js
@@ -1,53 +1,14 @@
-import { useState, useEffect, useReducer, useMemo } from 'react';
-
+import { useState, useEffect, useReducer, useMemo, useContext } from 'react';
+import {Context} from "./context"
 import Card from "./components/Card";
 import Layout from './components/Layout';
 import './App.css';
 
-const photos=[]
-const initialState={
-    items: photos,
-    count: photos.length,
-    inputs: {title:null, file:null, path:null},
-    isCollapsed: false
-}
-const handleOnChange=(state, e)=>{
-  if(e.target.name === 'file'){
-    return{...state.inputs, file: e.target.files[0], path:URL.createObjectURL(e.target.files[0])};
-  }else{
-    return{...state.inputs,title:e.target.value};
-  }
-}
 
-function reducer(state, action){
-  switch(action.type){
-    case 'setItem':
-      return{
-        ...state,
-        items:[state.inputs, ...state.items],
-        count: state.items.length+1,
-        inputs:{title:null, file:null, path:null}
-      }
-
-    case 'setInputs':
-      return{
-        ...state,
-        inputs:handleOnChange(state, action.payload.value)
-      }
-    case 'collapse':
-      return{
-        ...state,
-        isCollapsed: action.payload.bool
-      }
-      default: return state;
-  }
-}
 
 function App() {
-  const [state, dispatch]= useReducer(reducer,initialState);
-
-  
-
+ 
+  const {dispatch, state} =useContext(Context);
   const toggle=(bool)=> dispatch({type: "collapse", payload:{bool}});
   const handleOnChange=(e)=> dispatch({type: 'setInputs', payload: {value:e}});
  
@@ -68,8 +29,9 @@ function App() {
     onSubmit={handleOnSubmit}
     toggle={toggle}
     >
-        {count}
+        
         <h1 className='text-center'>Gallery</h1>
+        {count}
         <div className='row'>
           {/* {Array.apply(null,{length: 9}).map(()=> <Card />)} */}
           {state.items.map((item, index)=> <Card key={index} {...item}/>)}
---
+++ react_courses/src/context.js
@@ -0,0 +1,46 @@
+import { createContext, useReducer } from "react";
+export const Context=createContext()
+const photos=[]
+const initialState={
+    items: photos,
+    count: photos.length,
+    inputs: {title:null, file:null, path:null},
+    isCollapsed: false
+}
+const handleOnChange=(state, e)=>{
+  if(e.target.name === 'file'){
+    return{...state.inputs, file: e.target.files[0], path:URL.createObjectURL(e.target.files[0])};
+  }else{
+    return{...state.inputs,title:e.target.value};
+  }
+}
+
+function reducer(state, action){
+  switch(action.type){
+    case 'setItem':
+      return{
+        ...state,
+        items:[state.inputs, ...state.items],
+        count: state.items.length+1,
+        inputs:{title:null, file:null, path:null}
+      }
+
+    case 'setInputs':
+      return{
+        ...state,
+        inputs:handleOnChange(state, action.payload.value)
+      }
+    case 'collapse':
+      return{
+        ...state,
+        isCollapsed: action.payload.bool
+      }
+      default: return state;
+  }
+}
+
+const Provider=({children})=>{
+    const [state, dispatch]= useReducer(reducer,initialState);
+    return <Context.Provider value={{state,dispatch}}>{children}</Context.Provider> //Allow any component to  access Context to send actions through dispatch to reduce() and reduce() would return upgrade(new) state.
+}
+export default Provider;
