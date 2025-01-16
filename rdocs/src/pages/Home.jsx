c14f6d364861335c9fce3b595205ec05d2f714b2
Sojib Ali <sojib.ali@hotmail.com>
2025-01-16 13:27:13 +0600

    added dropdown effect

--- rdocs/src/pages/Home.jsx
+++ rdocs/src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import "../styles/home.css";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { useState } from "react";
+
 export function Home() {
+  const [IsShow, setIsShow] = useState(false);
   return (
     <>
     {/* <div className="lessons">
@@ -14,15 +18,31 @@ export function Home() {
       <Link to="/context-image-list" className="nav-link">
         Replace prop drilling with context
       </Link>
+      <IoIosArrowDropdownCircle />
       
     </div> */}
+   
+      <h3
+        style={{
+          display:"inline-block",
+          paddingRight: "0.2rem"
+        
+        }}
+      >Adding interactivity - lesson(2)</h3>
+      <span><IoIosArrowDropdown 
+        onClick={()=>{
+          setIsShow(!IsShow);
+        }}
+      /></span>
+      {IsShow &&
     <div>
-      <h3>Adding interactivity - lesson(2)</h3>
       <Link to="/next-gallery" className="nav-link">Next Gallery app</Link>
       <Link to="/input-form" className="nav-link">Input form</Link>
       <Link to="/textarea-msg" className="nav-link">Textarea alert message</Link>
       <Link to="/alert-input" className="nav-link">Alert input</Link>
+
     </div>
+}
     </>
   );
 }
