65174d3dd4f6960771f5edc5f6c592691646dfb8
Sojib Ali <sojib.ali@hotmail.com>
2025-01-19 22:08:12 +0600

    completed SelectForm page

--- rdocs/src/topics/AddInteractivity/Lesson_4/SelectForm.jsx
+++ rdocs/src/topics/AddInteractivity/Lesson_4/SelectForm.jsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+
 const SelectForm = () => {
+  const [message, setMessage] = useState("");
+  const [person, setPerson] = useState("Lukas Kyle");
+
+  const handleSend = (e) => {
+    e.preventDefault();
+    alert(`${message} to ${person}`);
+  };
   return (
     <>
-      <form action="">
+      <form onSubmit={handleSend}>
         <label htmlFor="person">To:</label>
-        <select name="" id="">
-          <option value="Alice">Alice</option>
-          <option value="Bob">Bob</option>
+        <select name="" id="" onChange={(e) => setPerson(e.target.value)}>
+          <option value="Lukas Kyle">Lukas Kyle</option>
+          <option value="Juliette Nicholas">Juliette Nicholas</option>
+          <option value="solo">Solo</option>
         </select>
         <br /> <br />
-        <textarea name="" id=""></textarea>
+        <textarea
+          name=""
+          id=""
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
         <br />
-        <input type="submit" value="send" />
+        {/* <input type="submit" value="send" onClick={handleSend} /> */}
+        <button type="submit">Send</button>
       </form>
     </>
   );
