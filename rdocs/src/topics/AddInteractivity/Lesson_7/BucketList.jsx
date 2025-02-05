ddfa0189d485d01cc0f2303a761bbcb7e5819545
Sojib Ali <sojib.ali@hotmail.com>
2025-02-05 20:05:12 +0600

    dealt with nested array

--- rdocs/src/topics/AddInteractivity/Lesson_7/BucketList.jsx
+++ rdocs/src/topics/AddInteractivity/Lesson_7/BucketList.jsx
@@ -1,27 +1,69 @@
 import { useState } from "react";
 
-const BucketList = () => {
-  const initialList = [
-    { id: 1, name: "Big Bellies" },
-    { id: 2, name: "Lunar Landscape" },
-    { id: 3, name: "Terracotta Army" },
-  ];
+const initialList = [
+  { id: 0, title: "Big Bellies", seen: false },
+  { id: 1, title: "Lunar Landscape", seen: false },
+  { id: 2, title: "Terracotta Army", seen: true },
+];
 
-  const [listItem, setListItem] = useState(initialList);
+const BucketList = () => {
+  const [myList, setMyList] = useState(initialList);
+  const [yourList, setYourList] = useState(initialList);
+
+  function handleMyList(artworkId, nextSeen) {
+    setMyList(
+      myList.map((list) => {
+        if (list.id === artworkId) {
+          return { ...list, seen: nextSeen };
+        } else {
+          return list;
+        }
+      })
+    );
+  }
+
+  function handleYourList(artworkId, nextSeen) {
+    setYourList(
+      yourList.map((list) => {
+        if (list.id === artworkId) {
+          return { ...list, seen: nextSeen };
+        } else {
+          return list;
+        }
+      })
+    );
+  }
 
   return (
     <>
       <h1>Art Bucket List</h1>
       <h2>My list of art to see: </h2>
-      <ul>
-        {listItem.map((item) => (
-          <li key={item.id}>
-            <input type="checkbox" />
-            <label htmlFor="">{item.name}</label>
-          </li>
-        ))}
-      </ul>
+      <ItemList artworks={myList} onToggle={handleMyList} />
+
+      <h2>Your list of art to see:</h2>
+      <ItemList artworks={yourList} onToggle={handleYourList} />
     </>
+  );
+};
+
+const ItemList = ({ artworks, onToggle }) => {
+  return (
+    <ul>
+      {artworks.map((item) => (
+        <li key={item.id}>
+          <label htmlFor="">
+            <input
+              type="checkbox"
+              checked={item.seen}
+              onChange={(e) => {
+                onToggle(item.id, e.target.checked);
+              }}
+            />
+            {item.title}
+          </label>
+        </li>
+      ))}
+    </ul>
   );
 };
 
