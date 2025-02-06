f1fe15190779cb91c4d6fdfe55ee87f7f6531dd6
Sojib Ali <sojib.ali@hotmail.com>
2025-02-06 21:31:10 +0600

    added dynamic input field to set the player name

--- udemy/react_exercises/src/projects/project_2/components/Player.jsx
+++ udemy/react_exercises/src/projects/project_2/components/Player.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+
 const Player = ({ name, symbol }) => {
+  const [isPressed, setIspressed] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  function handleClick() {
+    // !isPressed ? setIspressed(true) : setIspressed(false);
+    setIspressed((pressed) => !pressed);
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        <span className="player-name">
+          {isPressed ? (
+            <input
+              type="text"
+              onChange={(e) => setPlayerName(e.target.value)}
+              value={playerName}
+            />
+          ) : (
+            playerName
+          )}
+        </span>
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleClick}>{isPressed ? "save" : "edit"}</button>
     </li>
   );
 };
