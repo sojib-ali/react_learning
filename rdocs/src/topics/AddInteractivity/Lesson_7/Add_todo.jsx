3f80c1a50ee628b0a0f5176b862bc1e1640709da
Sojib Ali <sojib.ali@hotmail.com>
2025-02-11 20:20:11 +0600

    added 2 more components in Add_todo

--- rdocs/src/topics/AddInteractivity/Lesson_7/Add_todo.jsx
+++ rdocs/src/topics/AddInteractivity/Lesson_7/Add_todo.jsx
@@ -9,8 +9,9 @@ const initialList = [
 const Add_todo = () => {
   const [newItems, setNewItems] = useState("");
   const [listItems, setListItems] = useState(initialList);
-  const [editingId, setEditingId] = useState(null);
-  const [saveItems, setSaveItems] = useState("");
+  // const [listItems, setListItems] = useState(initialList);
+  // const [editingId, setEditingId] = useState(null);
+  // const [saveItems, setSaveItems] = useState("");
 
   function addClick() {
     setListItems((item) => [
@@ -20,6 +21,65 @@ const Add_todo = () => {
     setNewItems("");
   }
 
+  // function removeItems(itemId) {
+  //   setListItems(listItems.filter((item) => item.id !== itemId));
+  // }
+
+  // function editItem(itemId) {
+  //   if (editingId === itemId) {
+  //     setListItems(
+  //       listItems.map((item) =>
+  //         item.id === itemId ? { ...item, listName: saveItems } : item
+  //       )
+  //     );
+  //     setEditingId(null);
+  //     setSaveItems("");
+  //   } else {
+  //     setEditingId(itemId);
+  //     const currentItem = listItems.find((item) => item.id === itemId);
+  //     setSaveItems(currentItem.listName);
+  //   }
+  // }
+  return (
+    <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          onChange={(e) => setNewItems(e.target.value)}
+          value={newItems}
+          placeholder="add item"
+        />{" "}
+        <button onClick={addClick}>Add</button>
+      </form>
+
+      {/* <ProductList onRemove={removeItems} onEdit={editItem} /> */}
+      <AddFunction listItems={listItems} setListItems={setListItems} />
+      {/* <ul>
+        {listItems.map((listItem) => (
+          <li key={listItem.id}>
+            {editingId === listItem.id ? (
+              <input
+                type="text"
+                onChange={(e) => setSaveItems(e.target.value)}
+                value={saveItems}
+              />
+            ) : (
+              listItem.listName
+            )}
+            <button onClick={() => editItem(listItem.id)}>
+              {editingId === listItem.id ? "Save" : "Edit"}
+            </button>{" "}
+            <button onClick={() => removeItems(listItem.id)}>Delete</button>
+          </li>
+        ))}
+      </ul> */}
+    </>
+  );
+};
+
+function AddFunction({ listItems, setListItems }) {
+  const [editingId, setEditingId] = useState(null);
+  const [saveItems, setSaveItems] = useState("");
   function removeItems(itemId) {
     setListItems(listItems.filter((item) => item.id !== itemId));
   }
@@ -39,18 +99,29 @@ const Add_todo = () => {
       setSaveItems(currentItem.listName);
     }
   }
+
+  return (
+    <ProductList
+      onRemove={removeItems}
+      onEdit={editItem}
+      listItems={listItems}
+      saveItems={saveItems}
+      setSaveItems={setSaveItems}
+      editingId={editingId}
+    />
+  );
+}
+
+function ProductList({
+  onRemove,
+  onEdit,
+  listItems,
+  saveItems,
+  setSaveItems,
+  editingId,
+}) {
   return (
     <>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="text"
-          onChange={(e) => setNewItems(e.target.value)}
-          value={newItems}
-          placeholder="add item"
-        />{" "}
-        <button onClick={addClick}>Add</button>
-      </form>
-
       <ul>
         {listItems.map((listItem) => (
           <li key={listItem.id}>
@@ -63,16 +134,16 @@ const Add_todo = () => {
             ) : (
               listItem.listName
             )}
-            <button onClick={() => editItem(listItem.id)}>
+            <button onClick={() => onEdit(listItem.id)}>
               {editingId === listItem.id ? "Save" : "Edit"}
             </button>{" "}
-            <button onClick={() => removeItems(listItem.id)}>Delete</button>
+            <button onClick={() => onRemove(listItem.id)}>Delete</button>
           </li>
         ))}
       </ul>
     </>
   );
-};
+}
 
 export default Add_todo;
 
