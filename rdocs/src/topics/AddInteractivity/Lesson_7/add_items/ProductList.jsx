3d04f53b83358ddc03e27677a2c789604370bd5a
Sojib Ali <sojib.ali@hotmail.com>
2025-04-13 16:44:33 +0600

    completed to do list

--- rdocs/src/topics/AddInteractivity/Lesson_7/add_items/ProductList.jsx
+++ rdocs/src/topics/AddInteractivity/Lesson_7/add_items/ProductList.jsx
@@ -1,101 +1,124 @@
 import { useState } from "react";
-const ProductList = ({items, setItems}) => {
-  
-  const [editingId,setEditingId] = useState(null);
+const ProductList = ({ items, setItems }) => {
+  const [editingId, setEditingId] = useState(null);
   const [saveItems, setSaveItems] = useState("");
   const [isSelect, setIsSelect] = useState(false);
   const [selectedIds, setSelectedIds] = useState([]);
 
-  function handleDelete(productId){
+  function handleDelete(productId) {
     //cancelling during editing items
-    if(editingId === productId){
-      setItems(items.map((product)=>product.id === productId ? product : product))
+    if (editingId === productId) {
+      setItems(
+        items.map((product) => (product.id === productId ? product : product))
+      );
       setEditingId(null);
-    }else{
-    //removing an items
-      setItems(items.filter((product)=> product.id!== productId));
+    } else {
+      //removing an items
+      setItems(items.filter((product) => product.id !== productId));
     }
-    
   }
 
-  function handleSave(productId){
+  function handleSave(productId) {
     //updating items
-    if(editingId === productId){
-      setItems(items.map((product)=>product.id == productId ? {...product, listName:saveItems}: product)) 
+    if (editingId === productId) {
+      setItems(
+        items.map((product) =>
+          product.id == productId
+            ? { ...product, listName: saveItems }
+            : product
+        )
+      );
       setSaveItems("");
       setEditingId(null);
-    }else{
+    } else {
       // currentValue flows to saveItems so that input field can show the current items
-      let currentValue = items.find((value)=>value.id == productId)
+      let currentValue = items.find((value) => value.id == productId);
       setSaveItems(currentValue.listName);
       setEditingId(productId);
     }
   }
 
- function handleBoxChange(productId){
-    setSelectedIds(prev =>
-      prev.includes(productId)
-      ? prev.filter(itemId => itemId !== productId) //uncheck
-      : [...prev,productId] // putting check values intor the array
+  function handleBoxChange(productId) {
+    setSelectedIds(
+      (prev) =>
+        prev.includes(productId)
+          ? prev.filter((itemId) => itemId !== productId) //uncheck
+          : [...prev, productId] // putting check values intor the array
     );
   }
- function handleRemoveSelected(){
-    setItems(prev=> prev.filter(item=>!selectedIds.includes(item.id)));
+  function handleRemoveSelected() {
+    setItems((prev) => prev.filter((item) => !selectedIds.includes(item.id)));
     setSelectedIds([]);
   }
 
   //toggle select all
-  function handleAllSelect(){
+  function handleAllSelect() {
     selectedIds.length === items.length
-    ? setSelectedIds([])
-    : setSelectedIds(items.map((item)=>item.id))
+      ? setSelectedIds([])
+      : setSelectedIds(items.map((item) => item.id));
+  }
+
+  function handleRemoveAll() {
+    setItems([]);
+    setSelectedIds([]);
   }
   return (
     <>
-        {items.length == 0 ? (<p>Please add items to your list</p>):
-        (
-        <div style={{padding: "1rem"}}>
-        <button onClick={()=>setIsSelect(prev => !prev)}>select</button>
-        <button style={{marginInline:"0.5rem"}} 
-          onClick={handleAllSelect}
-          disable={!isSelect}
-        >{selectedIds.length === items.length ? "Deselect all" : "select all"} </button>
-        <button>remove all</button>{" "}
-        <button onClick={handleRemoveSelected} disabled={selectedIds.length===0}>remove selected item</button>
-     
-        <ul>
-            {items.map((product)=>(
-
-                <li key={product.id}>
-                  {isSelect && 
-                  <input 
+      {items.length == 0 ? (
+        <p>Please add items to your list</p>
+      ) : (
+        <div style={{ padding: "1rem" }}>
+          <button onClick={() => setIsSelect((prev) => !prev)}>select</button>
+          <button
+            style={{ marginInline: "0.5rem" }}
+            onClick={handleAllSelect}
+            disabled={!isSelect}
+          >
+            {selectedIds.length === items.length
+              ? "Deselect all"
+              : "select all"}{" "}
+          </button>
+          <button onClick={handleRemoveAll}>remove all</button>{" "}
+          <button
+            onClick={handleRemoveSelected}
+            disabled={selectedIds.length === 0}
+          >
+            remove selected item
+          </button>
+          <ul>
+            {items.map((product) => (
+              <li key={product.id}>
+                {isSelect && (
+                  <input
                     type="checkbox"
-                    checked={ selectedIds.includes(product.id)}
-                    onChange={()=>handleBoxChange(product.id)}
-                    />}{" "}
-                  
-                  {editingId === product.id? (                   
-                    <input type="text"
-                    onChange={(e)=>setSaveItems(e.target.value)}
-                    value={saveItems} />                  
-                  ) : (                 
-                   product.listName  
-                  )}{" "}
-                  <button onClick={()=>handleSave(product.id)}>
-                    {editingId === product.id ? "Save": "Edit"}
-                    </button>{" "}
-                  <button onClick={()=>handleDelete(product.id)}>
-                  {editingId === product.id ? "Cancel": "Delete"}
-                  </button>
-                </li>                             
-            ))} 
-        </ul> 
+                    checked={selectedIds.includes(product.id)}
+                    onChange={() => handleBoxChange(product.id)}
+                  />
+                )}{" "}
+                {editingId === product.id ? (
+                  <input
+                    type="text"
+                    onChange={(e) => setSaveItems(e.target.value)}
+                    value={saveItems}
+                  />
+                ) : (
+                  product.listName
+                )}{" "}
+                <button onClick={() => handleSave(product.id)}>
+                  {editingId === product.id ? "Save" : "Edit"}
+                </button>{" "}
+                <button onClick={() => handleDelete(product.id)}>
+                  {editingId === product.id ? "Cancel" : "Delete"}
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
-      )}       
+      )}
     </>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
 
 //make a add bar component
