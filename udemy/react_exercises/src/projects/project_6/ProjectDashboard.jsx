3927505941dacd2f4fdfb4985d1ae7e25e52b6b4
sojib <thesojibali@gmail.com>
2025-05-13 22:29:23 +0600

    designt form and fallback page

--- udemy/react_exercises/src/projects/project_6/ProjectDashboard.jsx
+++ udemy/react_exercises/src/projects/project_6/ProjectDashboard.jsx
@@ -1,11 +1,34 @@
 import NewProjects from "./components/NewProjects"
 import ProjectSidebar from "./components/ProjectSidebar"
+import NoProjectSelected from "./components/NoProjectSelected"
+import { useState } from "react"
 
 const ProjectDashboard = () => {
+
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects:[]
+  })
+
+  function handleStartAddProject(){
+    setProjectState((prevState)=>({
+      ...prevState,
+      selectedProjectId:null
+    }))
+  }
+
+  let content;
+  if (projectState.selectedProjectId === null){
+    content = <NewProjects />
+  }else if(projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-        <ProjectSidebar/>
-        <NewProjects />
+        <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+        {content}
     </main>
   )
 }
