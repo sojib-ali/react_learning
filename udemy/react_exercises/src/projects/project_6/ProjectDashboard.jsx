a692c23167525fb8e3a5f10c95f2b89df97e9be9
sojib <thesojibali@gmail.com>
2025-05-15 11:29:34 +0600

    project details can be seen now

--- udemy/react_exercises/src/projects/project_6/ProjectDashboard.jsx
+++ udemy/react_exercises/src/projects/project_6/ProjectDashboard.jsx
@@ -2,6 +2,7 @@ import NewProjects from "./components/NewProjects"
 import ProjectSidebar from "./components/ProjectSidebar"
 import NoProjectSelected from "./components/NoProjectSelected"
 import { useState } from "react"
+import SelectedProject from "./components/SelectedProject"
 
 const ProjectDashboard = () => {
 
@@ -10,10 +11,24 @@ const ProjectDashboard = () => {
     projects:[]
   })
 
+  function handleSelectProject(id){
+    setProjectState((prevState)=>({
+      ...prevState,
+      selectedProjectId:id
+    }))
+  }
+
   function handleStartAddProject(){
     setProjectState((prevState)=>({
       ...prevState,
       selectedProjectId:null
+    }))
+  }
+
+  function handleCancelProject(){
+    setProjectState((prevState)=>({
+      ...prevState,
+      selectedProjectId:undefined
     }))
   }
 
@@ -30,18 +45,37 @@ const ProjectDashboard = () => {
     }))
   }
 
+  function handleDeleteProject(){
+      setProjectState((prevState)=>({
+      ...prevState,
+      selectedProjectId:undefined,
+      projects: prevState.projects.filter(
+        project => project.id !== prevState.selectedProjectId
+      )
+    }))
+  }
 
+
+  const selectedProject = projectState.projects.find(
+    project => project.id === projectState.selectedProjectId
+  )
   let content;
   if (projectState.selectedProjectId === null){
-    content = <NewProjects onAdd={handleAddProject}/>
+    content = <NewProjects onAdd={handleAddProject} onCancel={handleCancelProject}/>
   }else if(projectState.selectedProjectId === undefined){
     content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
-
+  }else{
+    content =<SelectedProject project={selectedProject} onDelete = {handleDeleteProject} />
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-        <ProjectSidebar onStartAddProject={handleStartAddProject} projects = {projectState.projects} />
+        <ProjectSidebar 
+          onStartAddProject={handleStartAddProject} 
+          projects = {projectState.projects}
+          onSelectProject = {handleSelectProject}
+          selectedProjectId = {projectState.selectedProjectId}
+        />
         {content}
     </main>
   )
--- udemy/react_exercises/src/projects/project_6/components/NewProjects.jsx
+++ udemy/react_exercises/src/projects/project_6/components/NewProjects.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import Input from "./Input"
 import Modals from './Modals'
 
-const NewProjects = ({onAdd}) => {
+const NewProjects = ({onAdd, onCancel}) => {
   
   const[showModal, setShowModal] = useState(false);
 
@@ -46,7 +46,7 @@ const NewProjects = ({onAdd}) => {
       </Modals>}
       <div className="w-[35rem] mt-16">
           <menu className="flex items-center justify-end gap-4 my-4">
-              <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+              <li><button onClick={onCancel} className="text-stone-800 hover:text-stone-950">Cancel</button></li>
               <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
           </menu>
           <div>
