b246b70e508c97c2f885e1a53f1d77c5ca3765f3
sojib <sojib.ali@hotmail.com>
2025-05-18 10:25:37 +0600

    did commit composition on Shop component

--- udemy/react_exercises/src/projects/project_7/E_shop.jsx
+++ udemy/react_exercises/src/projects/project_7/E_shop.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './e_shop.css';
-
+import Product from './components/Product.jsx';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
-import { DUMMY_PRODUCTS } from './dummy-products.js';
+
 
 function E_shop() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -72,7 +72,13 @@ function E_shop() {
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
     </>
   );
 }
