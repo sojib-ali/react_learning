dd4e04284ce42e839ba5af5864babac042ca69e9
sojib <sojib.ali@hotmail.com>
2025-05-28 18:39:41 +0600

    removed the states from Quizzs and incorporated in the Quiz_app components

--- udemy/react_exercises/src/projects/project_9/Quiz_app.jsx
+++ udemy/react_exercises/src/projects/project_9/Quiz_app.jsx
@@ -10,18 +10,36 @@ const Quiz_app = () => {
     remainingTime: 0,
   });
 
-  function handleQuiz() {
-    if (quizIndex < quizData.length - 1) {
-      setQuizState({ ...quizIndex, quizIndex: quizIndex + 1 });
-    } else {
-      setQuizState({ quizIndex: 0 });
-    }
+  function handleNextQuiz() {
+    setQuizState((prevState) => {
+      let nextIndex;
+      if (prevState.quizIndex < quizData.length - 1) {
+        nextIndex = prevState.quizIndex + 1;
+      } else {
+        nextIndex = 0;
+      }
+      return {
+        ...prevState,
+        quizIndex: nextIndex,
+      };
+    });
+  }
+
+  function handlePrevQuiz() {
+    setQuizState((prevState) => ({
+      ...prevState,
+      quizIndex: prevState.quizIndex != 0 ? prevState.quizIndex - 1 : 0,
+    }));
   }
 
   return (
     <section>
       <QuizProgressBar timer={300} />
-      <Quizzs onNext={handleQuiz} index={quizState.quizIndex} />
+      <Quizzs
+        onNext={handleNextQuiz}
+        index={quizState.quizIndex}
+        onPrev={handlePrevQuiz}
+      />
     </section>
   );
 };
--- udemy/react_exercises/src/projects/project_9/components/Quizzs.jsx
+++ udemy/react_exercises/src/projects/project_9/components/Quizzs.jsx
@@ -1,25 +1,9 @@
 import { useState } from "react";
 import { quizData } from "../questions";
 
-const Quizzs = ({ onNext, index }) => {
-  //   const [index, setIndex] = useState(0);
-
+const Quizzs = ({ onNext, index, onPrev }) => {
   let quiz = quizData[index];
 
-  function handleNext() {
-    // if (index < quizData.length - 1) {
-    //   setIndex(index + 1);
-    // } else {
-    //   setIndex(0);
-    // }
-    onNext();
-  }
-
-  function handlePrev() {
-    if (index != 0) {
-      setIndex(index - 1);
-    }
-  }
   return (
     <>
       <h2> {quiz.question} </h2>
@@ -28,8 +12,8 @@ const Quizzs = ({ onNext, index }) => {
           <li key={key}>{option}</li>
         ))}
       </ul>
-      <button onClick={handleNext}>Next</button>{" "}
-      <button onClick={handlePrev}>Prev</button>
+      <button onClick={() => onNext()}>Next</button>{" "}
+      <button onClick={() => onPrev()}>Prev</button>
     </>
   );
 };
