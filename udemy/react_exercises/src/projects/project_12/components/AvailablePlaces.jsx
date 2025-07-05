746fc7201a535144ebb004efc73b2364266cd297
sojib <sojib.ali@hotmail.com>
2025-07-05 10:57:09 +0600

    added error handling on data fetching function

--- udemy/react_exercises/src/projects/project_12/components/AvailablePlaces.jsx
+++ udemy/react_exercises/src/projects/project_12/components/AvailablePlaces.jsx
@@ -1,23 +1,47 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message: error.message || "could not fetch data, try again later",
+        });
+      }
+
+      setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
 
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching text.."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
