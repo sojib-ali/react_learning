7776723dda5ec7f3c91c68f8c8316dbc16a15f78
sojib <sojib.ali@hotmail.com>
2025-07-09 21:02:53 +0600

    added separete state for each input field

--- udemy/react_exercises/src/projects/project_13/components/Login.jsx
+++ udemy/react_exercises/src/projects/project_13/components/Login.jsx
@@ -1,29 +1,53 @@
+import { useState } from "react";
+
 export default function Login() {
-  function handleSubmit() {
-    console.log("submitted");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("user email :" + enteredEmail);
+  }
+
+  function handleEmailChange(e) {
+    setEnteredEmail(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setEnteredPassword(e.target.value);
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+            value={enteredEmail}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+            value={enteredPassword}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button type="button" className="button" onClick={handleSubmit}>
-          Login
-        </button>
+        <button className="button">Login</button>
       </p>
     </form>
   );
