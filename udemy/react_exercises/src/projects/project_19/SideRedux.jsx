f82abff834da07e2b5772001e5ddd4fb7edade27
sojib <sojib.ali@hotmail.com>
2025-08-06 17:41:53 +0600

    sent data to firebase realtime database

--- udemy/react_exercises/src/projects/project_19/SideRedux.jsx
+++ udemy/react_exercises/src/projects/project_19/SideRedux.jsx
@@ -3,9 +3,29 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import "./sideRedux.css";
+import { useEffect } from "react";
 
 const SideRedux = () => {
   const showCart = useSelector((state) => state.ui.isCartVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      const response = await fetch(
+        "https://react-http-999c0-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+
+      const responseData = await response.json();
+    };
+  }, [cart]);
 
   return (
     <Layout>
