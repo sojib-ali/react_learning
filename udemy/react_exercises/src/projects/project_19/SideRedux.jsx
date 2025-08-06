dfb3f5aad611c3cd84011d697df658626aed8161
sojib <sojib.ali@hotmail.com>
2025-08-06 18:35:52 +0600

    dispatched notification alert for error or data send confirmation

--- udemy/react_exercises/src/projects/project_19/SideRedux.jsx
+++ udemy/react_exercises/src/projects/project_19/SideRedux.jsx
@@ -1,16 +1,30 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import "./sideRedux.css";
 import { useEffect } from "react";
+import { uiAction } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 const SideRedux = () => {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.isCartVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
     const sendCartData = async () => {
+      dispatch(
+        uiAction.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+
       const response = await fetch(
         "https://react-http-999c0-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
         {
@@ -23,15 +37,47 @@ const SideRedux = () => {
         throw new Error("Sending cart data failed");
       }
 
-      const responseData = await response.json();
+      dispatch(
+        uiAction.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data successfully!",
+        })
+      );
+
+      // const responseData = await response.json();
     };
-  }, [cart]);
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiAction.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 };
 
--- udemy/react_exercises/src/projects/project_19/store/ui-slice.js
+++ udemy/react_exercises/src/projects/project_19/store/ui-slice.js
@@ -2,10 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
     name: 'ui',
-    initialState: {isCartVisible: false},
+    initialState: {isCartVisible: false, notification: null},
     reducers:{
         toggle(state){
             state.isCartVisible = !state.isCartVisible
+        },
+        showNotification(state, action){
+           state.notification = {
+            status: action.payload.status,
+            title: action.payload.title,
+            message: action.payload.message,
+           }
         }
     }
 })
