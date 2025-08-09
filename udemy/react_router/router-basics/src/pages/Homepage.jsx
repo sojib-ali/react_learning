0770fe854b266278d1e75db59e15ddc5d2b12a78
sojib <sojib.ali@hotmail.com>
2025-08-09 21:04:18 +0600

    added Link form react-router

--- udemy/react_router/router-basics/src/pages/Homepage.jsx
+++ udemy/react_router/router-basics/src/pages/Homepage.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router";
+
 function Homepage() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>
+        Go to <Link to="/products">the list of products</Link>{" "}
+      </p>
+    </>
+  );
 }
 
 export default Homepage;
