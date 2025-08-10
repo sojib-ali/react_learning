441bd6d6d938adc592b7696aa0c576b585147cf1
sojib-ali <sojib.ali@hotmail.com>
2025-08-10 18:36:53 +0600

    modified Homepage

--- udemy/react_router/router-basics/src/pages/Homepage.jsx
+++ udemy/react_router/router-basics/src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 function Homepage() {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>yo yo</h1>
       <p>
         Go to <Link to="/products">the list of products</Link>{" "}
       </p>
