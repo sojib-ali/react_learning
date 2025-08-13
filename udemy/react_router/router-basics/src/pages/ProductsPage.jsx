48390aa394351cdf8ff6a3f097bb6fd518c98996
sojib-ali <sojib.ali@hotmail.com>
2025-08-13 21:19:12 +0600

    added relative path to go back siblings route

--- udemy/react_router/router-basics/src/pages/ProductsPage.jsx
+++ udemy/react_router/router-basics/src/pages/ProductsPage.jsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
+
 const ProductsPage = () => {
+  const PRODUCTS = [
+    { id: 1, item: "product 1" },
+    { id: 2, item: "product 2" },
+    { id: 3, item: "product 3" },
+  ];
+
   return (
     <>
       <h2>Products Page</h2>
-      <p>product 1</p>
-      <p>product 2</p>
-      <p>product 3</p>
+      <ul>
+        {PRODUCTS.map((prod) => (
+          <li key={prod.id}>
+            <Link to={`/products/${prod.id}`}> {prod.item} </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
