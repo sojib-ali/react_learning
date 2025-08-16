260a10b8fe9a7ff11ef01b29d693ca0598318fc1
sojib-ali <sojib.ali@hotmail.com>
2025-08-16 17:19:26 +0600

    added loader function to fetch data from the backend

--- udemy/react_router/router-advanced/src/App.jsx
+++ udemy/react_router/router-advanced/src/App.jsx
@@ -17,7 +17,20 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRoot />,
         children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch("http://localhost:8080/events");
+
+              if (!response.ok) {
+                //...
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            },
+          },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId", element: <EventDetails /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
--- udemy/react_router/router-advanced/src/pages/EventsPage.jsx
+++ udemy/react_router/router-advanced/src/pages/EventsPage.jsx
@@ -1,28 +1,14 @@
-import { Link } from "react-router-dom";
-
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    title: "some event",
-  },
-  {
-    id: "e2",
-    title: "another event",
-  },
-];
+import EventsList from "../components/EventsList";
+import { useLoaderData } from "react-router-dom";
 
 function EventsPage() {
+  const events = useLoaderData();
+
   return (
     <>
-      <h1>Events Page</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
 }
+
 export default EventsPage;
