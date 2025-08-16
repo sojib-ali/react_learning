c5d52c2767a5f94cf9670b8590d0ddbb8b0fb6d0
sojib-ali <sojib.ali@hotmail.com>
2025-08-16 16:08:08 +0600

    setup prettier locally for specifically react-router projects

--- udemy/react_router/router-advanced/src/pages/EventsPage.jsx
+++ udemy/react_router/router-advanced/src/pages/EventsPage.jsx
@@ -1,4 +1,28 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "some event",
+  },
+  {
+    id: "e2",
+    title: "another event",
+  },
+];
+
 function EventsPage() {
-  return <div>EventsPage</div>;
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 export default EventsPage;
