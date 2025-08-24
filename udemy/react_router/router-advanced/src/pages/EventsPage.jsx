5b3f810b87bc1a51033535e2fa01a76e329faa83
sojib-ali <sojib.ali@hotmail.com>
2025-08-24 17:28:38 +0600

    defered the loader with suspense fallback and Await

--- udemy/react_router/router-advanced/src/pages/EventsPage.jsx
+++ udemy/react_router/router-advanced/src/pages/EventsPage.jsx
@@ -1,24 +1,22 @@
+import { Suspense } from "react";
 import EventsList from "../components/EventsList";
-import { useLoaderData } from "react-router-dom";
+import { Await, useLoaderData } from "react-router-dom";
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
-
-  // if (data.isError) {
-  //   return <p>{data.message}</p>;
-  // }
+  const { events } = useLoaderData();
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading ...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -27,8 +25,14 @@ export async function loader() {
     });
     // return { isError: true, message: "could not fetch events." };
     //...
-  } else {
-    // const resData = await response.json();
-    return response;
   }
+
+  const data = await response.json();
+  return data.events ?? data;
+}
+
+export async function loader() {
+  return {
+    events: loadEvents(),
+  };
 }
