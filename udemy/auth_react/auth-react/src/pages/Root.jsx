3e39913c2376526ebf1ed426fb73d5d660423101
sojib-ali <sojib.ali@hotmail.com>
2025-08-27 17:12:17 +0600

    added automatic logout by getting the expiration time from backencd

--- udemy/auth_react/auth-react/src/pages/Root.jsx
+++ udemy/auth_react/auth-react/src/pages/Root.jsx
@@ -1,9 +1,31 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useNavigation, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(()=>{
+    if(!token){
+      return;
+    }
+
+    if(token === 'EXPIRED'){
+      submit(null, {action: '/logout', mehtod: 'post'})
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(()=>{
+      submit(null, {action: '/logout', mehtod: 'post'})
+    },tokenDuration);
+  },[token, submit])
 
   return (
     <>
