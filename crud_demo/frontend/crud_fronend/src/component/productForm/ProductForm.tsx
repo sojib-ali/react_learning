d0eab0dac1694c9659aafe0cbfbaf8b30a31be5a
sojib-ali <sojib.ali@hotmail.com>
2025-09-13 15:18:05 +0600

    tested printing form values using formData

--- crud_demo/frontend/crud_fronend/src/component/productForm/ProductForm.tsx
+++ crud_demo/frontend/crud_fronend/src/component/productForm/ProductForm.tsx
@@ -1,13 +1,45 @@
+import React, { useState } from "react";
+
+type ProductData = {
+    title: string;
+    description: string;
+}
+
 const ProductForm = () => {
+    const[inputData, setInputData] = useState <ProductData | null> (null);
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget)
+        const data = Object.fromEntries(formData) as ProductData;
+        setInputData(data);
+        e.currentTarget.reset();
+    }
+
     return (
-        <form>
+        <>
+        
+        <form onSubmit={handleSubmit}>
             <label htmlFor="">Product name:</label>
-            <input type="text" /> <br /> <br />
+            <input type="text" name="title" defaultValue=''/> <br /> <br />
 
             <label htmlFor="">Product details:</label>
-            <input type="text" />
+            <textarea name = "description" /> <br /> <br />
             <button type="submit">Submit</button>
         </form>
+
+       
+            {inputData && (
+                <div>
+                    <h3>Submitted Data:</h3>
+                    <p>Title: {inputData.title}</p>
+                    <p>Description: {inputData.description}</p>
+                </div>
+            )}
+
+        
+        </>
+        
     )
 }
 export default ProductForm
