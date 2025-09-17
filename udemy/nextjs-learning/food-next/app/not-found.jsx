41cdcd9fae7cf7924004e2c9ef8b2291959063b6
sojib-ali <sojib.ali@hotmail.com>
2025-09-17 22:55:24 +0600

    added error, loading, suspense

---
+++ udemy/nextjs-learning/food-next/app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return(
+        <main>
+            <h1>Not found</h1>
+            <h1>Unfortunately, we could not find the requested page or resource.</h1>
+        </main>
+    )
+}
