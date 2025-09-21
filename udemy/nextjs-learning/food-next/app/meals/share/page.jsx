f965aa3e60bfddb32962f4f26fadcf73232ee417
sojib-ali <sojib.ali@hotmail.com>
2025-09-21 23:06:35 +0600

    added useFormSTatus to get the current state of the form submission

--- udemy/nextjs-learning/food-next/app/meals/share/page.jsx
+++ udemy/nextjs-learning/food-next/app/meals/share/page.jsx
@@ -1,6 +1,7 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/action';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
     
@@ -44,7 +45,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name = "image"/>
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
--- udemy/nextjs-learning/food-next/lib/action.jsx
+++ udemy/nextjs-learning/food-next/lib/action.jsx
@@ -1,14 +1,37 @@
 'use server'
+import { saveMeal } from "./meals"
+import { RedirectType } from "next/navigation";
+
+function isInvalidText(text){
+    return !text || text.trim() === '';
+}
 
 export async function shareMeal(formData){        
         const meal = {
             title: formData.get('title'),
             summary: formData.get('summary'),
-            instruction: formData.get('instruction'),
+            instructions: formData.get('instructions'),
             image: formData.get('image'),
             creator: formData.get('name'),
             creator_email: formData.get('email')
+        };
+
+        if(isInvalidText(meal.title) ||
+            isInvalidText(meal.title) ||
+            isInvalidText(meal.summary) ||
+            isInvalidText(meal.instructions) ||
+            isInvalidText(meal.creator) ||
+            isInvalidText(meal.creator_email) ||
+            !meal.creator_email.includes('@') ||
+            !meal.image ||
+            meal.image.size === 0
+
+        ){
+            return{
+                message: 'Invalid input'
+            }
         }
     
-        console.log(meal);
+        await saveMeal(meal);
+        RedirectType('/meals');
     }
